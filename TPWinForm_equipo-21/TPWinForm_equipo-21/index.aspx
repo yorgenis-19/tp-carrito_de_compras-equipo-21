37cec2f528b3e54a9bb75cdcadb2565f2a08142f
Dante Beltran Nanziot <dantebelnan@gmail.com>
2023-10-10 23:44:40 -0300

    Logica inicial del carrito + falta arreglar que no toma los valores

--- TPWinForm_equipo-21/TPWinForm_equipo-21/index.aspx
+++ TPWinForm_equipo-21/TPWinForm_equipo-21/index.aspx
@@ -2,17 +2,24 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-
-    <asp:GridView ID="dgvArticulos" CssClass="table" AutoGenerateColumns="false" runat="server">
+    <style>
+        .hidden{
+            display:none;
+        }
+    </style>
+    <asp:GridView runat="server" ID="dgvArticulos" CssClass="table" OnSelectedIndexChanged="dgvArticulos_SelectedIndexChanged" AutoGenerateColumns="false" DataKeyNames="id">
         <Columns>
+            <asp:BoundField HeaderText="id" DataField="id"/>
             <asp:BoundField HeaderText="codigo" DataField="codigo" />
             <asp:BoundField HeaderText="nombre" DataField="nombre" />
             <asp:BoundField HeaderText="descripcion" DataField="descripcion" />
             <asp:BoundField HeaderText="marca" DataField="marca" />
             <asp:BoundField HeaderText="categoria" DataField="categoria" />
-            <asp:BoundField HeaderText="precio" DataField="precio" />
-
+            <asp:BoundField HeaderText="precio" DataField="precio"  />
+            <asp:CommandField ShowSelectButton="true" SelectText="Agregar al Carrito" HeaderText="Acción"/>
+            
         </Columns>
     </asp:GridView>
 
+    
 </asp:Content>
--- TPWinForm_equipo-21/TPWinForm_equipo-21/index.aspx.cs
+++ TPWinForm_equipo-21/TPWinForm_equipo-21/index.aspx.cs
@@ -15,9 +15,52 @@ namespace TPWinForm_equipo_21
         List<Articulo> articulos = new List<Articulo>();
         protected void Page_Load(object sender, EventArgs e)
         {
-            articulos = articuloService.listar();
-            dgvArticulos.DataSource = articulos;
+            dgvArticulos.DataSource = articuloService.listar();
             dgvArticulos.DataBind();
+            /*if (!IsPostBack)
+            {
+                articulos = articuloService.listar();
+                dgvArticulos.DataSource = articulos;
+                dgvArticulos.DataBind();
+                if (Session["Carrito"] is null)
+                {
+                    List<Articulo> carrito = new List<Articulo>();
+                    Session.Add("Carrito", carrito);
+                }
+
+            }*/
         }
+
+        protected void dgvArticulos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            /*int id = int.Parse(dgvArticulos.SelectedDataKey.Value.ToString());
+            Articulo newArticulo = new Articulo();
+            newArticulo = articuloService.buscarPorId(id);
+            if(!estaEnCarrito(newArticulo))
+            {
+                List<Articulo> carrito = new List<Articulo>();
+                carrito = (List<Articulo>)Session["Carrito"];
+                carrito.Add(newArticulo);
+            }*/
+
+            var test = dgvArticulos.SelectedRow.Cells[2];
+
+        }
+
+        private bool estaEnCarrito(Articulo articulo)
+        {
+            List<Articulo> carrito = new List<Articulo>();
+            carrito = (List<Articulo>)Session["Carrito"];
+
+            foreach (Articulo a in carrito)
+            {
+                if(a == articulo)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
     }
 }

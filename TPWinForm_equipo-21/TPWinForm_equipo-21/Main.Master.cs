70afc9e4928d9e5939404ce6d93745dc591fc46e
Dante Beltran Nanziot <dantebelnan@gmail.com>
2023-10-15 20:26:14 -0300

    Renderizado condicional de filtros evitando que aparezca en detalles y carrito

--- TPWinForm_equipo-21/TPWinForm_equipo-21/Main.Master.cs
+++ TPWinForm_equipo-21/TPWinForm_equipo-21/Main.Master.cs
@@ -11,7 +11,16 @@ namespace TPWinForm_equipo_21
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (Request.Url.AbsolutePath == "/index.aspx" || Request.Url.AbsolutePath == "/")
+            {
+                // Renderiza el contenido específico para la página de inicio o la ruta base
+                filtros.Visible = true; // Asegúrate de que miDiv sea un control en tu página
+            }
+            else
+            {
+                // Oculta o elimina el contenido si no estás en la página de inicio o la ruta base
+                filtros.Visible = false;
+            }
         }
     }
 }
